50085042fc99bf86f735a8e2f79e6e55242f584a
gasdiver <gasdiver@gmail.com>
2020-06-23 21:41:16 -0500

    Post Build

---
+++ cleanup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+##################################################################################################################
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+# Problem solving commands
+
+# Read before using it.
+# https://www.atlassian.com/git/tutorials/undoing-changes/git-reset
+# git reset --hard orgin/master
+# ONLY if you are very sure and no coworkers are on your github.
+
+sudo rm -r out/
+sudo rm -r work/
+
+
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
